7ab9aa2029ff33d415deb148f08b12dcd0f15b49
Jorge <jorge.attie@br.ey.com>
2021-11-07 22:55:20 -0300

    menu mobile finalizado porem sem funcionalidades pagina home 100% responsiva

--- src/utils/script.js
+++ src/utils/script.js
@@ -1,22 +1,56 @@
-//select class
-const menu = document.querySelector(".checkbtn");
-const close = document.querySelector(".checkbtn2 ");
-console.log(menu);
-function myFunction() {
-  var element = document.getElementById("checkbtn");
-  element.classList.add("mystyle");
+// //select class
+// const menu = document.querySelector(".checkbtn");
+// const close = document.querySelector(".checkbtn2 ");
+// console.log(menu);
+// function myFunction() {
+//   var element = document.getElementById("checkbtn");
+//   element.classList.add("mystyle");
+// }
+// menu.addEventListener(
+//   "click",
+//   myFunction(() => {
+//     console.log("clicou");
+//     console.log("aqui", close);
+
+//     console.log("aqui2", close);
+//   })
+// );
+
+// close.addEventListener("click", function () {
+//   console.log("clicou");
+// });
+// console.log(menu);
+const btnMobile = document.getElementById("mobile");
+const btnClose = document.getElementById("close");
+
+function toggleMenu() {
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
 }
-menu.addEventListener(
-  "click",
-  myFunction(() => {
-    console.log("clicou");
-    console.log("aqui", close);
-
-    console.log("aqui2", close);
-  })
-);
-
-close.addEventListener("click", function () {
-  console.log("clicou");
+function myFunction() {
+  var myobj = document.getElementById("mobile");
+  const close = document.getElementById("close");
+  close.classList.toggle("open");
+  myobj.classList.toggle("close");
+}
+function closeMenu() {
+  var element = document.getElementById("nav");
+  element.classList.remove("active");
+  const close = document.getElementById("close");
+  close.classList.remove("open");
+  var myobj = document.getElementById("mobile");
+  myobj.classList.remove("close");
+}
+btnMobile.addEventListener("click", () => {
+  myFunction();
+  toggleMenu();
 });
-console.log(menu);
+// btnMobile.addEventListener("touchstart", () => {
+//   toggleMenu(), myFunction();
+// });
+btnClose.addEventListener("click", () => {
+  closeMenu();
+});
