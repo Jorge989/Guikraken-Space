9dfcf29f0d49a9c2f794ac0dbd70dc0a7b9450f0
Jorge <jorge.attie@br.ey.com>
2021-12-02 15:18:00 -0300

    fixing bugs

--- src/utils/script.js
+++ src/utils/script.js
@@ -1,25 +1,3 @@
-// //select class
-// const menu = document.querySelector(".checkbtn");
-// const close = document.querySelector(".checkbtn2 ");
-// console.log(menu);
-// function myFunction() {
-//   var element = document.getElementById("checkbtn");
-//   element.classList.add("mystyle");
-// }
-// menu.addEventListener(
-//   "click",
-//   myFunction(() => {
-//     console.log("clicou");
-//     console.log("aqui", close);
-
-//     console.log("aqui2", close);
-//   })
-// );
-
-// close.addEventListener("click", function () {
-//   console.log("clicou");
-// });
-// console.log(menu);
 const btnMobile = document.getElementById("mobile");
 const btnClose = document.getElementById("close");
 
